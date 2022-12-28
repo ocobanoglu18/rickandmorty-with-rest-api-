c0d978f728b4e74461e31c5ca7912c85f147fc6c
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2022-12-28 17:39:18 +0300

    Initial Commit

---
+++ R&MRest/R_MRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_MRestApp.swift
+//  R&MRest
+//
+//  Created by Adnan Cobanoglu on 28.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct R_MRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
